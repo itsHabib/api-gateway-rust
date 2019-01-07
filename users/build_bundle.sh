3f012db6441c40d1a7ac96f56606196f6505cc2a
Michael Habib <mhabib@pixability.com>
2019-01-06 19:37:29 -0500

    create scripts to bundle zip file while building image

---
+++ users/build_bundle.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+commit_hash=$(git rev-parse --short HEAD)
+project_dir=$(pwd)
+rm -f rust.zip
+
+if [ "$1" = --keep-release ]; then
+    echo "bundling project.."
+    # create temporarily rename dockerignore 
+    mv -f .dockerignore .dockerignore-$commit_hash
+    docker build -f Dockerfile.bundle -t rust-lambda:bundle \
+    --build-arg commit_hash=$commit_hash .
+    mv -f .dockerignore-$commit_hash .dockerignore
+
+    echo "retrieving bundle and release dir.."
+    mkdir -p $project_dir/target/release # just in case
+    # copy release dir and bundle
+    id=$(docker run -d rust-lambda:bundle tail -f /dev/null)
+    docker cp $id:/bundles-$commit_hash/rust.zip $project_dir
+    docker cp $id:/app/target/release $project_dir/target/
+    docker stop $id && docker rm $id
+    
+    echo "complete!"
+
+else
+    echo "bundling project from scratch.."
+    docker build -f Dockerfile.bundle -t rust-lambda:bundle \
+    --build-arg commit_hash=$commit_hash .
+
+    echo "retrieving bundle.."
+    id=$(docker run -d rust-lambda:bundle tail -f /dev/null)
+    docker cp $id:/bundles-$commit_hash/rust.zip $project_dir
+    docker stop $id && docker rm $id
+
+    echo "complete!"
+fi
+
--- users/update_lambda.sh
+++ users/update_lambda.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-rm -rf rust.zip
-echo "zipping binary.."
-zip -j rust.zip target/release/bootstrap
-
 echo "updating code.."
 aws lambda update-function-code --function-name $1 --zip-file fileb://rust.zip
 
