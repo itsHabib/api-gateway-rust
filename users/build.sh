ce96d61b45c777f2d67787c83be869d8f8e20b88
Michael Habib <mhabib@pixability.com>
2019-01-12 20:22:44 -0500

    fix build and update script

--- users/build.sh
+++ users/build.sh
@@ -5,7 +5,7 @@ rm -f rust.zip
 
 echo "bundling project.."
 
-docker build -t rust-lambda:bundle --build-arg COMMIT_HASH=$commit_hash \
+docker build -t rust-lambda:bundle --build-arg COMMIT_HASH=$COMMIT_HASH \
 --build-arg AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
 --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
 --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY .
--- users/update_lambda.sh
+++ users/update_lambda.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "updating code.."
-aws lambda update-function-code --function-name $1 --zip-file fileb://rust.zip
 
+echo "updating code.."
+aws lambda update-function-code --function-name $1 --s3-bucket lambda-rust-builds \
+--s3-key build-$(git rev-parse --short HEAD)
